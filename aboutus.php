0235f298cafd4d74200564f07bbf9337f52a9726
XJ <xijie629629@gmail.com>
2023-01-11 17:24:03 +0800

    added about.us page by afrina and clinic search page 70% done

---
+++ aboutus.php
@@ -0,0 +1,142 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>FYP - ToothScanner</title>
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.3/examples/headers/">
+    <link href="dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="a.css" rel="stylesheet">
+  </head>
+  <body>
+    
+
+
+<main>
+  <h1 class="visually-hidden">About Us</h1>
+
+  <div class="container">
+    <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+      <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+
+        <span class="fs-4">ToothScanner</span>
+      </a>
+
+      <ul class="nav nav-pills">
+        <li class="nav-item"><a href="#" class="nav-link">Home</a></li>
+        <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">About Us</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Contact Us</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Register</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Login</a></li>
+      </ul>
+
+  
+  <div class="container my-5">
+    <div class="row p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-3 border shadow-lg">
+      <div class="col-lg-7 p-3 p-lg-5 pt-lg-3">
+        <h1 class="display-4 fw-bold lh-1">About ToothScanner</h1>
+        <p class="lead">ToothScanner is a one-stop solution to all your dental needs. We want you to have all the options at your fingertips. Therefore, we are here to provie you will all the dental options with best price and flexibility.</p>
+        <div class="d-grid gap-2 d-md-flex justify-content-md-start mb-4 mb-lg-3">
+          <button type="button" class="btn btn-primary btn-lg px-4 me-md-2 fw-bold">Login</button>
+          <button type="button" class="btn btn-outline-secondary btn-lg px-4">Register</button>
+        </div>
+      </div>
+      <div class="col-lg-4 offset-lg-1 p-0 overflow-hidden shadow-lg">
+          <img class="rounded-lg-3" src="aboutusdental.jpg" alt="" width="720">
+      </div>
+    </div>
+  </div>
+    </header>
+  </div> 
+
+<div class="container px-4 py-5">
+
+
+
+      <div class="col">
+        <div class="row row-cols-1 row-cols-sm-2 g-4">
+          <div class="col d-flex flex-column gap-2">
+
+            <h4 class="fw-semibold mb-0">Quality</h4>
+            <p class="text-muted">To deliver you the best and high-quality oral care </p>
+          </div>
+
+          <div class="col d-flex flex-column gap-2">
+
+            <h4 class="fw-semibold mb-0">Freedom of choice</h4>
+            <p class="text-muted">Choice is yours to decide as we provide a wide variety of services that are available</p>
+          </div>
+
+          <div class="col d-flex flex-column gap-2">
+
+            <h4 class="fw-semibold mb-0">Team</h4>
+            <p class="text-muted">Only experienced and dedicated team </p>
+          </div>
+
+          <div class="col d-flex flex-column gap-2">
+
+            <h4 class="fw-semibold mb-0">Service</h4>
+            <p class="text-muted">Prioritizing accessibility in all services we provide to you </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>  
+  </body>
+</html>
+
+
+<style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+
+      .b-example-divider {
+        height: 3rem;
+        background-color: rgba(0, 0, 0, .1);
+        border: solid rgba(0, 0, 0, .15);
+        border-width: 1px 0;
+        box-shadow: inset 0 .5em 1.5em rgba(0, 0, 0, .1), inset 0 .125em .5em rgba(0, 0, 0, .15);
+      }
+
+      .b-example-vr {
+        flex-shrink: 0;
+        width: 1.5rem;
+        height: 100vh;
+      }
+
+      .bi {
+        vertical-align: -.125em;
+        fill: currentColor;
+      }
+
+      .nav-scroller {
+        position: relative;
+        z-index: 2;
+        height: 2.75rem;
+        overflow-y: hidden;
+      }
+
+      .nav-scroller .nav {
+        display: flex;
+        flex-wrap: nowrap;
+        padding-bottom: 1rem;
+        margin-top: -1px;
+        overflow-x: auto;
+        text-align: center;
+        white-space: nowrap;
+        -webkit-overflow-scrolling: touch;
+      }
+    </style>
