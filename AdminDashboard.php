6e1097c24f4ec42527ec64892f70b0012856168e
unknown <afrinarazin@gmail.com>
2023-02-11 16:31:34 +0800

    add

--- AdminDashboard.php
+++ AdminDashboard.php
@@ -93,27 +93,34 @@
       <div class="position-sticky pt-3 sidebar-sticky">
         <ul class="nav flex-column">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">
+            <a class="nav-link active" aria-current="page" href="AdminDashboard.php">
               <span data-feather="home" class="align-text-bottom"></span>
               Admin Dashboard
             </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">
+            <a class="nav-link" href="AdminVisitationHist.php">
+              <span data-feather="pie-chart" class="align-text-bottom"></span>
+              Visitation History
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="AdminAcctMgmt.php">
               <span data-feather="settings" class="align-text-bottom"></span>
               Account Management
             </a>
           </li>
+         
           <li class="nav-item">
-            <a class="nav-link" href="#">
-              <span data-feather="thermometer" class="align-text-bottom"></span>
-              Treatments
+            <a class="nav-link" href="AdminBillsnPayment.php">
+              <span data-feather="dollar-sign" class="align-text-bottom"></span>
+              Bills And Payments
             </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">
-              <span data-feather="dollar-sign" class="align-text-bottom"></span>
-              Bills And Payments
+            <a class="nav-link" href="AdminClinicInformation.php">
+              <span data-feather="home" class="align-text-bottom"></span>
+              Clinic Information
             </a>
           </li>
           <li class="nav-item">
@@ -123,7 +130,7 @@
             </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">
+            <a class="nav-link" href="AdminRegisterClinic.php">
               <span data-feather="plus" class="align-text-bottom"></span>
               Register Clinic
             </a>
