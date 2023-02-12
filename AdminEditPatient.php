79eaa6eaab9cf58cf45388758723f9dd1765a83a
XJ <76989683+Rookie1231@users.noreply.github.com>
2023-02-12 13:39:21 +0800

    hh

--- AdminEditPatient.php
+++ AdminEditPatient.php
@@ -147,55 +147,9 @@ $successMessage="";
 
 <div class="container-fluid">
   <div class="row">
-    <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
-      <div class="position-sticky pt-3 sidebar-sticky">
-        <ul class="nav flex-column">
-          <li class="nav-item">
-          <a class="nav-link" href="AdminDashboard.php">
-              <span data-feather="home" class="align-text-bottom"></span>
-              Admin Dashboard
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="AdminVisitationHist.php">
-              <span data-feather="pie-chart" class="align-text-bottom"></span>
-              Visitation History
-            </a>
-          </li>
-          <li class="nav-item">
-           <a class="nav-link active" aria-current="page" href="AdminAcctMgmt.php">
-              <span data-feather="settings" class="align-text-bottom"></span>
-              Account Management
-            </a>
-          </li>
-          
-          <li class="nav-item">
-            <a class="nav-link" href="AdminBillsnPayment.php">
-              <span data-feather="dollar-sign" class="align-text-bottom"></span>
-              Bills And Payments
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="AdminClinicInformation.php">
-              <span data-feather="home" class="align-text-bottom"></span>
-              Clinic Information
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              <span data-feather="clock" class="align-text-bottom"></span>
-              View Time Slots
-            </a>
-          </li>
-          <li class="nav-item">
-          <a class="nav-link" href="AdminRegisterClinic.php">
-              <span data-feather="plus" class="align-text-bottom"></span>
-              Register Clinic
-            </a>
-          </li>
-        </ul>   
-      </div>
-    </nav>
+    <?php
+        require_once("AdminNav.php");
+    ?>
 
 
 
